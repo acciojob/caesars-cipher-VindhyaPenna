c676519e7ced6afec6f744570bca5619c2764882
VindhyaPenna <118455671+VindhyaPenna@users.noreply.github.com>
2023-10-12 11:45:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -41,8 +41,9 @@ function rot13(encodedStr) {
 		else
 			output+=encodedStr[i];
 	}
+	decodedArr.push(output)
 		
-  return output; //return decodedArr
+ return decodedArr
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
